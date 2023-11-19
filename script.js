e09287218be18156772b97ebd326e3e0a817dbbf
Marco <36082536+MarcoHijacker@users.noreply.github.com>
2023-11-19 12:44:56 +0100

    First commit.

---
+++ script.js
@@ -0,0 +1,21 @@
+//! M1 - Epicode - JavaScript Extra Training (Marco Longo)
+
+//* ARRAY:
+//? Ex.1 (Easy) - Realizzare un programma che costruisca un array di 20 elementi. Ogni elemento dev'essere pari al proprio indice nell'array.
+// Es. [0, 1, 2, 3, 4, 5 ... 19]
+
+//? Ex.2 (Medium) - Realizzare un programma che, dato un array di numeri interi, stampi in console la media di tutti i valori.
+// N.B. Utilizza solo i concetti visti a lezione, niente scorciatoie!
+
+//? Ex.3 (Medium) - Realizzare un programma che rimuova i multipli dispari di 3 dall'array riportato sotto.
+// N.B. 18 è un multiplo pari di 3, 21 è un multiplo dispari di 3.
+// let array = [20, 31, 15, 78, 48, 27, 61, 51]; 
+
+//? Ex.4 (Hard) - Dato un array di numeri interi (nums) ed un numero intero (target), ritornare i 2 indici degli elementi nell’array nums che, sommati, restituiscano il numero target.
+//? Assumere che ogni nums fornito in input accetti solo una soluzione e che questo contenga solo numeri diversi fra loro.
+// Esempio:
+// Input: nums = [4, 9, 11, 13] , target = 22
+// Output: [1,3]
+
+// nums [0, 1, 2, 3, 4, 5, 6]
+// Suggerimento: Sapevi che i cicli for possono essere innestati?
